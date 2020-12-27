b69650b3f5878ec939ff3431eb7436cd66770e74
cailinw <cailinwin@gmail.com>
2020-12-26 16:23:03 -0800

    pt lib dicts

--- Banyan/res/pt_lib.jl
+++ Banyan/res/pt_lib.jl
@@ -1,1 +1,31 @@
-begin end
+
+
+# PT Name --> LT_Name/Workers/Batches --> Implementation
+SPLIT_LIB_JL = Dict{String, Dict)()
+
+SPLIT_LIB_JL["None"]["Workers"] = function ()
+end
+
+SPLIT_LIB_JL["None"]["Batches"] = function ()
+end
+
+SPLIT_LIB_JL["None"]["Client"] = function ()
+end
+
+
+# PT Name --> LT_Name/Workers/Batches --> Implementation
+MERGE_LIB_JL = Dict{String, Dict}()
+
+MERGE_LIB_JL["None"]["Workers"] = function ()
+end
+
+MERGE_LIB_JL["None"]["Batches"] = function ()
+end
+
+MERGE_LIB_JL["None"]["Client"] = function ()
+end
+
+
+
+# Merge Name --> Split Name --> Implementation
+CAST_LIB_JL = Dict{String, Dict}()
