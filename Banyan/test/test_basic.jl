7b75d2a7d13b080c32014c7d7a1696010c47a99d
cailinw <cailinwin@gmail.com>
2021-01-09 03:56:56 +0000

    edits

--- Banyan/test/test_basic.jl
+++ Banyan/test/test_basic.jl
@@ -6,15 +6,15 @@
 #    destroy_job()
 #end
 
-@testset "Simple annotation" begin
-    x = Future(49)
-    # @pa x, Dict(x => "Mut"), PartitionAnnotation(Partitions(Dict(x.value_id => [Value(x)])), PartitioningConstraints(Set())) begin
-    @pa x, Dict(x => "Mut"), pa_noconstraints(Dict(x.value_id => [Value(x)])) begin
-        z = 10
-        println("hello ", z)
-    end
-    evaluate(x)
-end
+#@testset "Simple annotation" begin
+#    x = Future(49)
+#    # @pa x, Dict(x => "Mut"), PartitionAnnotation(Partitions(Dict(x.value_id => [Value(x)])), PartitioningConstraints(Set())) begin
+#    @pa x, Dict(x => "Mut"), pa_noconstraints(Dict(x.value_id => [Value(x)])) begin
+#        z = 10
+#        println("hello ", z)
+#    end
+#    evaluate(x)
+#end
 
 # @testset "Location Type" begin
 #     x = Future([1, 2, 3, 4, 5])
