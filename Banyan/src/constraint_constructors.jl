930c45fcfd956e96e5f4302bae5e00901962ea77
cailinw <cailinwin@gmail.com>
2021-01-10 18:48:19 -0800

    Cross constructor

--- Banyan/src/constraint_constructors.jl
+++ Banyan/src/constraint_constructors.jl
@@ -1,3 +1,12 @@
-function Cross(fut, )
-    return PartitioningConstraint
+function Cross(args...)
+    args = collect(args)
+    for i in 1::size(args, 1)
+        if !(typeof(args[i]) <: Tuple)
+            args[i] = (args[i], 0)
+        end
+    end
+    return PartitioningConstraint(
+        "Cross",
+        args
+    )
 end
