36762acca10f32d7f33ea43ec1d8b3e146353dbf
Caleb Winston <calebhwin@gmail.com>
2021-01-23 13:30:25 -0800

    Add example

--- Banyan/test/test_pa.jl
+++ Banyan/test/test_pa.jl
@@ -9,7 +9,7 @@ crossed = Cross(data, other)
 crossed = Cross((data, 0), (data, 1))
 
 # Creates a Task
-simple_pa = @pa mut data Block() x Value() where [crossed, ordered]
+simple_pa = @pa {mut data Block() x Value()} where [crossed, ordered]
 
 # Merges Tasks together by merging their PA unions
 @pp [simple_pa, unsorted_pa] begin
@@ -17,3 +17,14 @@ simple_pa = @pa mut data Block() x Value() where [crossed, ordered]
 end
 
 @dst data S3(".....")
+
+@pt data Block(0)
+@pt x Replicate()
+@pc Cross(data, other)
+@pa
+
+@partitioned begin
+    
+end
+
+@pa
