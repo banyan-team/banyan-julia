499330b24adc0babb73df7d9292415d12041f566
cailinw <cailinwin@gmail.com>
2021-02-06 21:21:51 +0000

    clusters

--- Banyan/src/clusters.jl
+++ Banyan/src/clusters.jl
@@ -1,4 +1,5 @@
 using Base64
+using JSON
 
 function encode_config_file(config_file_path)
     f = open(config_file_path, "r")
@@ -11,16 +12,20 @@ end
 
 function create_cluster(cluster_id, config_file_path)
     @debug "Creating cluster"
-    config_file_path = joinpath(@__DIR__, "../res/pcluster_config2")
+    config_file_path = joinpath(@__DIR__, "../res/pcluster_config_test")
     pcluster_config = encode_config_file(config_file_path)
     # TODO: Load pt_lib_info correctly
+    # TODO: Parameterize this function...
     send_request_get_response(
         :create_cluster,
 	Dict(
-            "cluster_id" => cluster_id,
-	    "language" => "jl",
-	    "pt_lib_info" => Dict(),
-	    "pcluster_config" => pcluster_config
+	     "cluster_id" => cluster_id,
+	     "name" => "BanyanTest",
+	     "ec2_key_pair" => "EC2ConnectKeyPairTest",
+	     "pcluster_additional_policy" => "arn:aws:iam::338603620317:policy/pcluster-additional-policy",
+	     "num_nodes" => 2,
+	     "instance_type" => "c5.2xlarge",
+	     "banyanfile" => JSON.json(Dict("language" => "jl", "cluster" => Dict("packages" => ["DataFrames"], "pt_lib_info" => Dict("splits" => Dict(), "merges" => Dict(), "casts" => Dict())), "job" => Dict()))
 	)
     )
 
