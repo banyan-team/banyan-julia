872ea0a8ed2b080e535accdc302c8078b353dbf6
Caleb Winston <calebhwin@gmail.com>
2021-08-06 20:10:14 -0700

    Format

--- BanyanDataFrames/test/test_io.jl
+++ BanyanDataFrames/test/test_io.jl
@@ -4,19 +4,48 @@
         iris = read_csv("s3://{bucket}/iris.csv")
         @test collect(nrow(iris)) == 150
         @test collect(ncol(iris)) == 5
-        @test collect(names(iris)) == ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
-        @test collect(propertynames(iris)) == [:sepal_length, :sepal_width, :petal_length, :petal_width, :species]
-        @test collect(first(iris)) == DataFrameRow(DataFrame(sepal_length=5.1, sepal_width=3.5, petal_length=1.4, petal_width=0.2, species="setosa"), 1)
-        @test collect(last(iris)) == DataFrameRow(DataFrame(sepal_length=5.9, sepal_width=3.0, petal_length=5.1, petal_width=1.8, species="virginica"), 1)
+        @test collect(names(iris)) ==
+              ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
+        @test collect(propertynames(iris)) ==
+              [:sepal_length, :sepal_width, :petal_length, :petal_width, :species]
+        @test collect(first(iris)) == DataFrameRow(
+            DataFrame(
+                sepal_length = 5.1,
+                sepal_width = 3.5,
+                petal_length = 1.4,
+                petal_width = 0.2,
+                species = "setosa",
+            ),
+            1,
+        )
+        @test collect(last(iris)) == DataFrameRow(
+            DataFrame(
+                sepal_length = 5.9,
+                sepal_width = 3.0,
+                petal_length = 5.1,
+                petal_width = 1.8,
+                species = "virginica",
+            ),
+            1,
+        )
 
-	# Append row and write to file
-	push!(iris, (1.0, 2.0, 3.0, 4.0, "newspecies"))
-	write_csv(iris, "s3://{bucket}/iris_new.csv")
+        # Append row and write to file
+        push!(iris, (1.0, 2.0, 3.0, 4.0, "newspecies"))
+        write_csv(iris, "s3://{bucket}/iris_new.csv")
         # Read file and verify
-	iris_new = read_csv("s3://{bucket}/iris_new.csv")
-	@test collect(nrow(iris_new)) == 151
-	@test collect(ncol(iris_new)) == 5
-	@test collect(last(iris)) == DataFrameRow(DataFrame(sepal_length=1.0, sepal_width=2.0, petal_length=3.0, petal_width=4.0, species="newspecies"), 1)
+        iris_new = read_csv("s3://{bucket}/iris_new.csv")
+        @test collect(nrow(iris_new)) == 151
+        @test collect(ncol(iris_new)) == 5
+        @test collect(last(iris)) == DataFrameRow(
+            DataFrame(
+                sepal_length = 1.0,
+                sepal_width = 2.0,
+                petal_length = 3.0,
+                petal_width = 4.0,
+                species = "newspecies",
+            ),
+            1,
+        )
     end
     try
         s3_delete(get_cluster_s3_bucket_name(get_cluster().name), "iris_new.csv")
--- BanyanDataFrames/test/test_small_dataset.jl
+++ BanyanDataFrames/test/test_small_dataset.jl
@@ -4,36 +4,40 @@
         iris = read_csv("s3://{bucket}/iris.csv")
 
         # Filter
-        iris_filtered = filter(row -> (row.sepal_length > 5.0 && row.sepal_width < 3.0), iris)
+        iris_filtered =
+            filter(row -> (row.sepal_length > 5.0 && row.sepal_width < 3.0), iris)
         num_rows = collect(nrows(iris_filtered))
- 	@test num_rows == 51
+        @test num_rows == 51
         unique_species = collect(unique(iris[!, :species]))
         @test unique_species == ["setosa", "versicolor", "virginica"]
         num_nonunique = collect(sum(nonunique(iris)))
         @test num_nonunique == 3
     end
 
-    run_with_job("Sorting")
+    run_with_job("Sorting") do job
         bucket = get_cluster_s3_bucket_name(get_cluster().name)
         iris = read_csv("s3://{bucket}/iris.csv")
-        
+
         # Sort
-	new = sort(iris, :sepal_width)
-	@test collect(first(new))[:sepal_width] == 2.0
+        new = sort(iris, :sepal_width)
+        @test collect(first(new))[:sepal_width] == 2.0
         @test collect(first(new))[:petal_length] == 3.5
         @test collect(last(new))[:sepal_width] == 4.4
         @test collect(last(new))[:petal_length] == 1.5
     end
 
-    run_with_job("Join and Groupby Aggregration")
+    run_with_job("Join and Groupby Aggregration") do job
         # Join and groupby aggregration
         bucket = get_cluster_s3_bucket_name(get_cluster().name)
         iris = read_csv("s3://{bucket}/iris.csv")
-        species_info = DataFrame(species=["setosa", "versicolor", "virginica"], region=["Arctic", "Eastern Canada", "Southeastern United States"])
-        new = innerjoin(iris, species_info, on=:species)
+        species_info = DataFrame(
+            species = ["setosa", "versicolor", "virginica"],
+            region = ["Arctic", "Eastern Canada", "Southeastern United States"],
+        )
+        new = innerjoin(iris, species_info, on = :species)
         ncols = collect(ncol(new))
         @test ncol == 6
-        
+
         gdf_species = groupby(new, :species)
         gdf_region = groupby(new, :region)
 
@@ -42,7 +46,7 @@
 
         @test g_cols = [:region]
         @test v_cols == [:sepal_length, sepal_width, :petal_length, :petal_width, :species]
-        
+
         lengths_species = collect(combine(gdf_species, :petal_length => mean))
         counts_species = collect(combine(gdf_region, nrow))
         length_region = collect(combine(gdf_region, :petal_length => mean))
@@ -52,24 +56,25 @@
         @test first(counts_species) == first(counts_region)
     end
 
-    run_with_job("Grouping and Mapping")
+    run_with_job("Grouping and Mapping") do job
         bucket = get_cluster_s3_bucket_name(get_cluster().name)
         iris = read_csv("s3://{bucket}/iris.csv")
 
         # Select
-        iris_sepal_area = select(iris, 1, 2, [:sepal_length, :sepal_width] => (a, b) -> a .* b)
+        iris_sepal_area =
+            select(iris, 1, 2, [:sepal_length, :sepal_width] => (a, b) -> a .* b)
         first_row = collect(first(iris_sepal_area))
         last_row = collect(last(iris_sepal_area))
         @test first_row[:sepal_length_sepal_width_function] == 10.0
         @test last_row[:sepal_length_sepal_width_function] == 30.02
 
         # Transform
-        iris_new = transform(gdf, :species => x -> "iris-".*x, keepkeys=false)
+        iris_new = transform(gdf, :species => x -> "iris-" .* x, keepkeys = false)
         species_names = collect(unique(iris_new[!, "species_function"]))
         @test species_names == ["iris-setosa", "iris-versicolor", "iris-virginica"]
 
         # Split-Apply-Combine
-	gdf = groupby(iris, :species)
+        gdf = groupby(iris, :species)
         iris_mins = collect(combine(gdf, :petal_length => minimum))
         @test iris_mins[!, :petal_length_minimum] == [1.0, 3.0, 4.5]
     end
