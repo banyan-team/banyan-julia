d771e14cf08ee89a2af9d3f585a43e664dde72c3
cailinw <cailinwin@gmail.com>
2021-08-29 06:12:21 +0000

    Fixed setup_stress

--- BanyanDataFrames/test/test_groupby_filter_indexing.jl
+++ BanyanDataFrames/test/test_groupby_filter_indexing.jl
@@ -153,18 +153,27 @@ function setup_stress_tests(bucket_name)
             "tripdata_large_arrow.arrow/tripdata_$(month)_copy1.arrow",
         )
         for ncopy = 2:n_repeats
-            cp(
-	        S3Path("s3://$(bucket)/tripdata_large_csv.csv/tripdata_$(month)_copy1.csv", config=Banyan.get_aws_config()),
-		S3Path("s3://$(bucket)/tripdata_large_csv.csv/tripdata_$(month)_copy$(ncopy).csv", config=Banyan.get_aws_config())
-	    )
-            cp(
-	        S3Path("s3://$(bucket)/tripdata_large_parquet.parquet/tripdata_$(month)_copy1.parquet", config=Banyan.get_aws_config()),
-		S3Path("s3://$(bucket)/tripdata_large_parquet.parquet/tripdata_$(month)_copy$(ncopy).parquet", config=Banyan.get_aws_config())
-	    )
-            cp(
-	        S3Path("s3://$(bucket)/tripdata_large_arrow.arrow/tripdata_$(month)_copy1.arrow", config=Banyan.get_aws_config()),
-		S3Path("s3://$(bucket)/tripdata_large_arrow.arrow/tripdata_$(month)_copy$(ncopy).arrow", config=Banyan.get_aws_config())
-	    )
+            dst_path = "s3://$(bucket)/tripdata_large_csv.csv/tripdata_$(month)_copy$(ncopy).csv"
+            if !s3_exists(Banyan.get_aws_config(), bucket, dst_path)
+                cp(
+	            S3Path("s3://$(bucket)/tripdata_large_csv.csv/tripdata_$(month)_copy1.csv", config=Banyan.get_aws_config()),
+		    S3Path(dst_path, config=Banyan.get_aws_config())
+	        )
+            end
+            dst_path = "s3://$(bucket)/tripdata_large_parquet.parquet/tripdata_$(month)_copy$(ncopy).parquet"
+            if !s3_exists(Banyan.get_aws_config(), bucket, dst_path)
+                cp(
+	            S3Path("s3://$(bucket)/tripdata_large_parquet.parquet/tripdata_$(month)_copy1.parquet", config=Banyan.get_aws_config()),
+		    S3Path(dst_path, config=Banyan.get_aws_config())
+	        )
+            end
+            dst_path = "s3://$(bucket)/tripdata_large_arrow.arrow/tripdata_$(month)_copy$(ncopy).arrow"
+            if !s3_exists(Banyan.get_aws_config(), bucket, dst_path)
+                cp(
+	            S3Path("s3://$(bucket)/tripdata_large_arrow.arrow/tripdata_$(month)_copy1.arrow", config=Banyan.get_aws_config()),
+		    S3Path(dst_path, config=Banyan.get_aws_config())
+	        )
+            end
         end
     end
 end
