ef17710cfc35f0994e9a9b24053949f74e0321aa
Caleb Winston <calebhwin@gmail.com>
2021-09-23 07:26:56 -0700

    Migrate tests for BA.jl

--- BanyanArrays/test/runtests.jl
+++ BanyanArrays/test/runtests.jl
@@ -1,4 +1,5 @@
 using ReTest
+using HDF5
 
 global jobs_for_testing = Dict()
 
@@ -33,9 +34,47 @@ function use_job_for_testing(;
     configure_scheduling(name = scheduling_config_name)
 end
 
+global data_for_testing = false
+
+function use_data(data_src="S3")
+    global data_for_testing
+
+    if !data_for_testing && data_src == "S3"
+        original = h5open(
+            download(
+                "https://support.hdfgroup.org/ftp/HDF5/examples/files/exbyapi/h5ex_d_fillval.h5",
+            ),
+        )
+        with_downloaded_path_for_reading(
+            joinpath(
+                S3Path(get_cluster_s3_bucket_name(), config = Banyan.get_aws_config()),
+                "fillval.h5",
+            ),
+            for_writing = true,
+        ) do f
+            new = h5open(string(f), "w")
+            new["DS1"] = repeat(original["DS1"][:, :], 100, 100)
+            close(new)
+        end
+        close(original)
+
+        # rm(get_s3fs_path(joinpath(get_cluster_s3_bucket_name(), "fillval_copy.h5")), force=true)
+        rm(
+            joinpath(
+                S3Path(get_cluster_s3_bucket_name(), config = Banyan.get_aws_config()),
+                "fillval_copy.h5",
+            ),
+            force = true,
+        )
+        # TODO: Maybe fsync here so that the directory gets properly updated
+    end
+
+    data_for_testing = true
+end
+
 include("mapreduce.jl")
-# include("hdf5.jl")
-# include("bs.jl")
+include("hdf5.jl")
+include("bs.jl")
 
 try
     runtests(Regex.(ARGS)...)
