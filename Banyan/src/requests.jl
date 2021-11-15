00354ccd1f474a42e662e2e47ead31068f9622ff
cailinw <cailinwin@gmail.com>
2021-11-14 17:35:02 -0800

    Wait for job before sending evaluate

--- Banyan/src/requests.jl
+++ Banyan/src/requests.jl
@@ -326,6 +326,11 @@ function send_evaluation(value_id::ValueId, job_id::JobId)
     global encourage_parallelism_with_batches
     global exaggurate_size
 
+    # First, ensure that the job is running
+    wait_for_job(job_id)
+
+    @debug "Sending evaluation request"
+
     # Get list of the modules used in the code regions here
     used_packages = union(vcat([req.task.used_modules for req in get_job().pending_requests if req isa RecordTaskRequest]...))
 
