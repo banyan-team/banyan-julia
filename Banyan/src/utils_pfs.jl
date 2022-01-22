3505c4f3675f46ec3e6142ed6478c5f3e09399b2
Caleb Winston <calebhwin@gmail.com>
2022-01-21 23:00:17 -0800

    Add debug print statements

--- Banyan/src/utils_pfs.jl
+++ Banyan/src/utils_pfs.jl
@@ -33,9 +33,9 @@ split_len(src_len, batch_idx::Integer, nbatches::Integer, comm::MPI.Comm) = spli
     get_npartitions(nbatches, comm),
 )
 
-Banyan.split_on_executor(src, d, i) = error("Splitting $(typeof(src)) not supported")
+split_on_executor(src, d, i) = error("Splitting $(typeof(src)) not supported")
 
-Banyan.split_on_executor(
+split_on_executor(
     src::T,
     dim::Integer,
     batch_idx::Integer,
@@ -58,7 +58,7 @@ Banyan.split_on_executor(
     end
 end
 
-Banyan.merge_on_executor(obj::Any; key = nothing) = error("Merging $(typeof(obj)) not supported")
+merge_on_executor(obj::Any; key = nothing) = error("Merging $(typeof(obj)) not supported")
 
 # # TODO: Make `merge_on_executor` and `tobuf` and `frombuf`
 # # dispatch based on the `kind` so we only have to precompile Arrow if we are
