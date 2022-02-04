1342b852acb0d16731e109a463ce03bf91cddd5b
cailinw <cailinwin@gmail.com>
2022-02-03 20:37:06 -0800

    Enable download on all workers in getpath

--- Banyan/src/utils_pfs.jl
+++ Banyan/src/utils_pfs.jl
@@ -466,22 +466,22 @@ function getpath(path, comm)
         # TODO: Add option for Internet locations as to whether or not to
         # cache on disk
         hashed_path = string(hash(path))
-        joined_path = "efs/banyan_dataset_" * hashed_path
+        joined_path = "efs/banyan_dataset_" * hashed_path * "_" * string(MPI.COMM_WORLD)
         # @info "Downloading $path to $joined_path"
         comm = MPI.COMM_WORLD
-        if MPI.Comm_rank(comm) == 0
-            println("On worker $(MPI.Comm_rank(comm)), $joined_path isfile = $(isfile(joined_path))")
-            if !isfile(joined_path)
-            # NOTE: Even though we are storing in /tmp, this is
-            # effectively caching the download. If this is undesirable
-            # to a user, a short-term solution is to use a different
-            # URL each time (e.g., add a dummy query to the end of the
-            # URL)
-                println("Going to download $path to $joined_path")
-                Downloads.download(path, joined_path)
-            end
+        # if MPI.Comm_rank(comm) == 0
+        println("On worker $(MPI.Comm_rank(comm)), $joined_path isfile = $(isfile(joined_path))")
+        if !isfile(joined_path)
+        # NOTE: Even though we are storing in /tmp, this is
+        # effectively caching the download. If this is undesirable
+        # to a user, a short-term solution is to use a different
+        # URL each time (e.g., add a dummy query to the end of the
+        # URL)
+            println("Going to download $path to $joined_path")
+            Downloads.download(path, joined_path)
         end
-        MPI.Barrier(comm)
+        # end
+        # MPI.Barrier(comm)
         println("Returning $joined_path on worker $(MPI.Comm_rank(comm))")
         # @show isfile(joined_path)
         joined_path
