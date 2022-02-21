8a1f6d6f50af41ce584b8ff4d40beb3032368b2f
Caleb Winston <calebhwin@gmail.com>
2022-02-20 20:54:38 -0800

    Fix CopyTo* barrier

--- BanyanArrays/src/pfs.jl
+++ BanyanArrays/src/pfs.jl
@@ -301,7 +301,7 @@ function CopyToJuliaArray(
         params["key"] = 1
         res = WriteJuliaArray(src, part, params, 1, 1, MPI.COMM_SELF, loc_name, loc_params)
     end
-    if Banyan.get_worker_idx(comm)
+    if get_worker_idx(comm) == 1
         MPI.Barrier(comm)
     end
 end
@@ -756,7 +756,7 @@ function CopyToHDF5(
         params["key"] = 1
         WriteHDF5(src, part, params, 1, 1, MPI.COMM_SELF, loc_name, loc_params)
     end
-    if Banyan.get_worker_idx(comm) == 1
+    if get_worker_idx(comm) == 1
         MPI.Barrier(comm)
     end
 end
