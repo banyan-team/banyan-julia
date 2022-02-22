77d2cd25b6135207562f2842d359557765e9736d
Caleb Winston <calebhwin@gmail.com>
2022-02-22 11:07:28 -0800

    Fix bug with usage of receive_from_client

--- Banyan/src/queues.jl
+++ Banyan/src/queues.jl
@@ -114,7 +114,7 @@ function receive_from_client(value_id)
         JSON.json(Dict("kind" => "SCATTER_REQUEST", "value_id" => value_id))
     )
     # Receive response from client
-    m = JSON.parse(get_next_message(get_scatter_queue()))
+    m = JSON.parse(get_next_message(get_scatter_queue())[1])
     v = from_jl_value_contents(m["contents"])
     v
 end
