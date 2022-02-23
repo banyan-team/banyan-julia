e09eb63f6d0d9fdf3d4f35df371089df136cc30a
Caleb Winston <calebhwin@gmail.com>
2022-02-23 06:18:02 -0800

    Add back in type annotations

--- Banyan/src/tasks.jl
+++ Banyan/src/tasks.jl
@@ -4,23 +4,23 @@
 
 mutable struct DelayedTask
     # Fields for use in processed task ready to be recorded
-    used_modules#::Vector
-    code#::String
-    value_names#::Vector{Tuple{ValueId,String}}
-    effects#::Dict{ValueId,String}
-    pa_union#::Vector{PartitionAnnotation} # Enumeration of applicable PAs
-    memory_usage#::Dict{ValueId,Dict{String,Integer}} # initial/additional/final
+    used_modules::Vector
+    code::String
+    value_names::Vector{Tuple{ValueId,String}}
+    effects::Dict{ValueId,String}
+    pa_union::Vector{PartitionAnnotation} # Enumeration of applicable PAs
+    memory_usage::Dict{ValueId,Dict{String,Integer}} # initial/additional/final
     # Fields for use in task yet to be processed in a call to `compute`
-    partitioned_using_func#::Union{Function,Nothing}
-    partitioned_with_func#::Union{Function,Nothing}
-    mutation#::Dict{Future,Future} # This gets converted to `effects`
+    partitioned_using_func::Union{Function,Nothing}
+    partitioned_with_func::Union{Function,Nothing}
+    mutation::Dict{Future,Future} # This gets converted to `effects`
     # Fields for estimating memory usage
-    inputs#::Vector{Future}
-    outputs#::Vector{Future}
-    scaled#::Vector{Future}
-    keep_same_sample_rate#::Bool
-    memory_usage_constraints#::Vector{PartitioningConstraint}
-    additional_memory_usage_constraints#::Vector{PartitioningConstraint}
+    inputs::Vector{Future}
+    outputs::Vector{Future}
+    scaled::Vector{Future}
+    keep_same_sample_rate::Bool
+    memory_usage_constraints::Vector{PartitioningConstraint}
+    additional_memory_usage_constraints::Vector{PartitioningConstraint}
 end
 
 DelayedTask() = DelayedTask(
