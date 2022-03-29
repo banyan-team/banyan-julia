b3ad6739cfc7292003c6a953c7caa3c1e6d745a0
Caleb Winston <calebhwin@gmail.com>
2022-03-28 22:25:02 -0700

    Fix type annotation for BanyanArrays.split_on_executor

--- BanyanArrays/src/utils_pfs.jl
+++ BanyanArrays/src/utils_pfs.jl
@@ -1,4 +1,4 @@
-Banyan.split_on_executor(src::AbstractArray, d::Int64, i::Int64) = selectdim(src, d, i)
+Banyan.split_on_executor(src::AbstractArray, d::Int64, i::UnitRange{Int64}) = selectdim(src, d, i)
 
 function Banyan.merge_on_executor(obj::Base.Vector{A}; key::Int64 = 1) where A <: Base.AbstractArray
     if length(obj) == 1
