27ac6c1fb671e84dea007af4aa24b55c22811c11
Caleb Winston <calebhwin@gmail.com>
2022-05-06 14:06:25 -0700

    Add debug statement

--- BanyanDataFrames/src/locations.jl
+++ BanyanDataFrames/src/locations.jl
@@ -170,6 +170,7 @@ function _remote_table_source(remotepath, shuffled, source_invalid, sample_inval
             sample_per_worker = gather_across(local_sample)
             @show sample_per_worker
             @show get_worker_idx()
+            error("hello")
             if is_main && !isempty(sample_per_worker)
                 vcat(sample_per_worker...), curr_meta_nrows
             else
