6cd59f1364cdb86509c10d0ac9a26a882b15bb04
Caleb Winston <calebhwin@gmail.com>
2022-05-06 14:59:21 -0700

    Add debug statements

--- BanyanDataFrames/src/locations.jl
+++ BanyanDataFrames/src/locations.jl
@@ -160,6 +160,11 @@ function _remote_table_source(remotepath, shuffled, source_invalid, sample_inval
         # Concatenate local samples and nrows together
         @show typeof((DataFrames.DataFrame(), Int64[]))
         @show gather_across(local_sample)
+        sample_per_worker = gather_across(local_sample)
+        @show typeof(sample_per_worker)
+        if is_main
+            @show (vcat(sample_per_worker...), curr_meta_nrows)
+        end
         error("hello there")
         remote_sample_value::DataFrames.DataFrame, meta_nrows_on_workers::Base.Vector{Int64} = if curr_parameters_invalid
             sample_and_meta_nrows_per_worker::Base.Vector{Tuple{DataFrames.DataFrame,Base.Vector{Int64}}} =
