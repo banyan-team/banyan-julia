117e72e8ada0d50d169941cbeda4fd1116d255a8
Caleb Winston <calebhwin@gmail.com>
2022-05-14 16:39:32 -0700

    Add debug statements

--- BanyanHDF5/src/locations.jl
+++ BanyanHDF5/src/locations.jl
@@ -294,6 +294,10 @@ function _remote_hdf5_source(path_and_subpath, shuffled, metadata_invalid, sampl
             nbytes,
             dset_sample,
         )
+        @show location_res
+        @show total_memory_usage(location_res)
+        @show total_memory_usage(location_res.sample)
+        @show total_memory_usage(location_res.sample.value)
         cache_location(remotepath, location_res, invalidate_sample, invalidate_metadata)
         location_res
     else
