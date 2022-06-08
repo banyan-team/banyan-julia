a245e516f64a7fb6cc0281afd59bd5ff2110f955
Caleb Winston <calebhwin@gmail.com>
2022-06-08 14:57:45 -0700

    Add debug statement

--- BanyanDataFrames/src/utils_pfs.jl
+++ BanyanDataFrames/src/utils_pfs.jl
@@ -68,8 +68,8 @@ function make_reducev_op(op)
         @show typeof(b)
         @show a
         @show b
-        a_df = get_variable_sized_blob(a) |> IOBuffer |> Arrow.Table |> DataFrames.DataFrame
-        b_df = get_variable_sized_blob(b) |> IOBuffer |> Arrow.Table |> DataFrames.DataFrame
+        a_df = get_variable_sized_blob(a) |> Base.collect |> IOBuffer |> Arrow.Table |> DataFrames.DataFrame
+        b_df = get_variable_sized_blob(b) |> Base.collect |> IOBuffer |> Arrow.Table |> DataFrames.DataFrame
         res_df = op(a_df, b_df)
         res_io = IOBuffer(sizehint=length(a))
         write(res_io, reinterpret(UInt8, Int64(1)))
@@ -85,7 +85,7 @@ function make_reducev_op(op)
     end
 end
 
-function reduce_buffers(a::Base.Vector{UInt8}, b::Base.Vector{UInt8})
+function reduce_buffers(a, b)
     @show a
     @show b
     a
